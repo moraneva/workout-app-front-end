ee56bdd77d796feeadacf2c24014d1efa382a372
Evan Moran <moraneva@msu.edu>
2017-01-14 07:53:39 -0500

    Cool way to make models

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -1,20 +1,8 @@
 export class Exercise {
 
-    id: number;
-    name: string;
-    picture: string;
-    moreInfoUrl: string;
-    equipment: string;
-    muscle: string;
-
-
-    constructor(exerciseInfo: any) {
-        this.id = exerciseInfo.id;
-        this.name = exerciseInfo.name;
-        this.picture = exerciseInfo.picture;
-        this.moreInfoUrl = exerciseInfo.moreInfoUrl;
-        this.equipment = exerciseInfo.equipment;
-        this.muscle = exerciseInfo.muscle;
+    constructor(public id: number, public name: string,
+     public picture: string, public moreInfoUrl: string,
+      public equipment: string, public muscle: string) {
     }
 
 }
